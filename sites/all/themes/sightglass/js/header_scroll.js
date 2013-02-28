0bd997703c1df6877bc16a818b5444d3009503f5
Erik <ehalber@gmail.com>
2013-02-27 21:35:37 -0500

    updated navigation

---
+++ sites/all/themes/sightglass/js/header_scroll.js
@@ -0,0 +1,29 @@
+(function ($) {
+  
+  $(document).ready(function() {
+
+    // Once the logo passes underneath the nav bar
+    // show the sightglass wave
+    
+    var winTop = '';
+
+    $(document).bind('load, scroll', function() {
+        winTop = $(window).scrollTop();
+        if (winTop >= 154) {
+            $('.separator').text('wave');
+        } else {
+            $('.separator').text('blank');
+        }
+    });
+
+    // Scroll the document back to top when the 
+    // user clicks on this element
+    $('.menu .separator').click(function() {
+        if (winTop >= 154) {
+            $('html,body').animate({scrollTop: 0 }, 'fast', 'easeOutQuad');
+        }
+    });
+
+  });
+  
+}(jQuery));
