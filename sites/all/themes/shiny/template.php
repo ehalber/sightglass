0aea5c8258012a415716972ad3bc15b3d222ade5
Erik <ehalber@gmail.com>
2013-03-22 11:20:56 -0400

    update views, shiny, libraries and commerce backoffice modules

--- sites/all/themes/shiny/template.php
+++ sites/all/themes/shiny/template.php
@@ -16,27 +16,6 @@ function shiny_views_bulk_operations_form_alter(&$form, &$form_state, $vbo) {
 }
 
 /**
- * Override or insert variables into the maintenance page template.
- */
-function shiny_preprocess_maintenance_page(&$vars) {
-  // While markup for normal pages is split into page.tpl.php and html.tpl.php,
-  // the markup for the maintenance page is all in the single
-  // maintenance-page.tpl.php template. So, to have what's done in
-  // shiny_preprocess_html() also happen on the maintenance page, it has to be
-  // called here.
-  shiny_preprocess_html($vars);
-  if (variable_get('install_task') != 'done') {
-    $footer_markup =  '<div class="message">' . t('Proudly built by') . '</div>';
-    $footer_markup .=  '<div class="logo">' . t('<a href="@url">Commerce Guys</a>', array('@url' => 'http://www.commerceguys.com')) . '</div>';
-    $vars['footer'] = array(
-      '#prefix' => '<div id="credit" class="clearfix">',
-      '#markup' => $footer_markup,
-      '#suffix' => '</div>',
-    );
-  }
-}
-
-/**
  * Override or insert variables into the html template.
  */
 function shiny_preprocess_html(&$vars) {
