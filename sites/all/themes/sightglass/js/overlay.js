5f5d258126e1fae735a3751e16c0459c59f4782e
Erik <ehalber@gmail.com>
2013-03-24 19:59:38 -0400

    updated slideshows

--- sites/all/themes/sightglass/js/overlay.js
+++ sites/all/themes/sightglass/js/overlay.js
@@ -18,11 +18,41 @@
     // This is used both in the blog and to show brewing tools
     // and good from the main catalog
     
-    $('.page-blog .views-field-field-image a, .node-type-article .views-field-field-image a').each(function(i,val) {
+    $('.page-blog .views-field-field-image a, .node-type-article .views-field-field-image a').each(function() {
         $(this).attr('href', $(this).find('img').attr('src'));
         var string = $(this).attr('href');
         $(this).attr('href', string.replace(/blog_hero/gi, 'lightbox_display'));  
         $(this).attr('title', $(this).find('img').attr('title')); 
+        
+        $('.fancybox').fancybox({
+            transitionIn: 'fade',
+            transitionOut: 'fade',
+            easeIn: 'easeInOutQuint',
+            easeOut: 'easeInOutQuint',
+            speedIn: 250,
+            speedOut: 400,
+            overlayOpacity: 1,
+            overlayColor: '#473000',
+            scrolling: 'no',
+            padding: 3,
+            cyclic: true,
+            titleFormat: function() {
+                return '<div class="image-title">' + $(this).attr('title') + '</div><div class="author">Photo by Jerad Jerad</div>';
+            },
+            titlePosition: 'outside',
+            onStart: function() {
+                //$('#fancybox-overlay').append('<div class="logo" />');
+                //$('.logo').css(logoCSS);
+                //$('.logo').css('left', $(window).width()/2 - 71);
+            },
+            onComplete: function() {
+                //$('body').css('overflow-y', 'hidden');
+            },
+            onClosed: function() {
+                //$('.logo').remove();
+               // $('body').css('overflow-y', 'scroll');
+            }
+        });
     });
 
     // TODO - this code should be wrapped in a function because
@@ -47,6 +77,7 @@
             overlayColor: '#473000',
             scrolling: 'no',
             padding: 3,
+            cyclic: true,
             titleFormat: function() {
                 return '<div class="image-title">' + $(this).attr('title') + '</div><div class="author">Photo by Jerad Jerad</div>';
             },
@@ -85,8 +116,9 @@
             overlayOpacity: 1,
             overlayColor: '#473000',
             scrolling: 'no',
-            padding: 0,
+            padding: 3,
             autoScale: true,
+            cyclic: true,
         
             titlePosition: 'outside',
             onStart: function() {
@@ -103,7 +135,46 @@
             }
         });
     });
-    
+
+
+    // Load fancy box when a slide is clicked on 
+    // any of the overview pages
+    $('.not-front .views-slideshow-cycle-main-frame-row').each(function() {
+        
+            var img = $(this).find('img').attr('src');
+            $(this).find('.fancybox').attr('href', img.replace(/tab_slideshow/gi, 'coffee_region_large'));
+            $(this).find('.fancybox').fancybox({
+            transitionIn: 'fade',
+            transitionOut: 'fade',
+            easeIn: 'easeInOutQuint',
+            easeOut: 'easeInOutQuint',
+            speedIn: 250,
+            speedOut: 400,
+            overlayOpacity: 1,
+            overlayColor: '#473000',
+            scrolling: 'no',
+            padding: 3,
+            autoScale: true,
+            cyclic: true,
+        
+            titlePosition: 'outside',
+            onStart: function() {
+                //$('#fancybox-overlay').append('<div class="logo" />');
+                //$('.logo').css(logoCSS);
+                //$('.logo').css('left', $(window).width()/2 - 71);
+            },
+            onComplete: function() {
+                //$('body').css('overflow-y', 'hidden');
+            },
+            onClosed: function() {
+                //$('.logo').remove();
+                //$('body').css('overflow-y', 'scroll');
+            }
+            });
+        
+        
+
+    });
 
     
 
