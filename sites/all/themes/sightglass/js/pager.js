3857676a6ad19a6a4f239222ded95b06ac0793fd
Erik <ehalber@gmail.com>
2013-04-22 10:26:56 -0400

    user account styling

--- sites/all/themes/sightglass/js/pager.js
+++ sites/all/themes/sightglass/js/pager.js
@@ -8,7 +8,31 @@
   
   $(document).ready(function() {
 
+  	/*
+  	  Change shopping cart to bag on every cart page
+  	 */
     $('.page-cart h1').html('<span>Shopping Bag</span>');
+
+    /*
+      Change page title from username to account on edit/view account pages
+     */
+    $('.page-user h1').html('<span>Account</span>');
+
+    /*
+      Change username to account on edit/view account pages
+     */
+    $('.page-user-edit h1').html('<span>Account</span>');
+
+    /*
+      Change page title to previous orders on order history page
+     */
+    $('.page-user-orders h1').html('<span>Previous Orders</span>');
+
+    /*
+      Change page title from usernae to address book
+     */
+    $('.page-user-addressbook h1').html('<span>Address Book</span>');
+
 
   });
   
