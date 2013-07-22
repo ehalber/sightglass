725f18271d17f39514cda2931216ef5e5c48da07
Laurentius Purba <lrpurba@yahoo.com>
2013-07-21 21:05:28 -0400

    Subscription CRUD, listing, Account CRUD and listing

---
+++ sites/all/modules/my_recurly/templates/my-recurly-list-accounts.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Prints out a summary of an existing credit card with masked digits.
+ */
+//echo '<pre>variables: ';print_r($variables);exit;
+//echo '<pre>variables: ';print_r($accounts);exit;
+// $output .= theme('table', array('header' => $header, 'rows' => $rows, 'attributes' => array('id' => 'permissions')));
+?>
+<?php //print theme('table', $variables); ?>
+<?php //dpm($variables); ?> 
+<?php //print theme('table', $accounts); ?>
+<!--
+<table>
+	<th><?php //print t('Plan Code'); ?></th>
+	<th><?php //print t('Quantity'); ?></th>
+	<th><?php //print t('Unit Price'); ?></th>
+</table>
+-->
+adfads
+
+<?php //if ($total > $per_page): ?>
+  <?php //print theme('pager'); ?>
+<?php //endif; ?>
